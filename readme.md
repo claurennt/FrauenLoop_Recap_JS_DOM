c3b52a920b80b542dcb8f2c8bc741d2f5f5824d4
Nathalie Scherf <nascherf@yahoo.se>
2021-02-23 16:36:12 +0100

    Add comments

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Practice your Dom-Manipulation skills!
+
+
+- add a button to the page. When you click the button, the sum of the numbers in the array should be calculated and displayed on the page. At the moment the sum is displayed all the time on the page, but you need to change this!
+
+- Try out: change the styling of your page using Dom-Manipulation. For this you can add another button and when you click on it, the style changes. For this you need to look into how to change (toggle) classes on an element using JS. 
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// different sytles of loops:
+// different styles of loops:
 
 let week = ['Monday', 'Tuesday', 'Wednesday']
 
 let people = ['Ann', 'Bert', 'Claus', 'David', 'Emma']
-// for -of 
+// for -of is a loop we can use only on arrays. 
 for (day of week) {
   console.log(day);
 }
@@ -11,17 +11,17 @@ for (day of week) {
 for (person of people) {
   console.log(person);
 }
-// for Each is a Method
+// for Each is a Method, which we can use on arrays.
 week.forEach(day => {
   console.log(day);
 })
 
-// more generall loop: for-loop
+// more generall loop: for-loop. This loop can be used to loop through Arrays, but also to repeat any code multiple times!
 for (let i = 100; i >= 10; i--){
-  // console.log(week[i]);
-  // console.log('index', i);
+  console.log('index', i);
 }
 
+// Two styles of functions: For now, you can use the one you like most!
 function myOldFunction () {
   return 'Hi, I am old school!'
 }
@@ -29,10 +29,11 @@ function myOldFunction () {
 const myNewFunction = () => {
   return "Hi! I'm modern!" 
 }
-
+// Both functions return something, i.e. they have a output. If we want to use the output of the function, we store it in a variable when we call the function:
 let result = myNewFunction(10,10,30)
 console.log(result);
 
+/** Challenges we did in class:*/
 // how can I display the numbers in the array on the page?
 let myNumbers = [30, 40, 55, 90]
 for (item of myNumbers) {
@@ -44,7 +45,7 @@ for (item of myNumbers) {
   let list = document.getElementsByClassName('numbers')
   list[0].append(listItem)
 }
-
+// here we do the same thig, display numbers, but use a different loop, just because we can!
 let myLuckyNumbers = [3,7,11,15,21,33]
 
 for (let index = 0; index < myLuckyNumbers.length ; index++){
@@ -53,9 +54,13 @@ for (let index = 0; index < myLuckyNumbers.length ; index++){
   let list = document.getElementsByClassName('numbers')
   list[0].append(listItem)
 }
+
+/** shorter way of insert more elements in the Html -document: */
+// Here I declare 2 variables and mix then into the text that is created in  line 64:
+
 let user = 'Nathalie'
 let action = `calculate the sum of Numbers`
-// shorter way of insert more elements in the Html -document: 
+
 let introText = document.createElement('div')
 introText.innerHTML = (
   `<h1>Welcome to ${user}'s page!</h1>
@@ -65,6 +70,7 @@ introText.innerHTML = (
 
 
 // how can I display the sum of the numbers on the page?
+// First calculate the sum:
 const calculateSum = (anArray) => {
   let sum = 0;
   for (number of anArray) {
@@ -72,7 +78,7 @@ const calculateSum = (anArray) => {
   }
   return sum
 }
-
+// store the return value of the function, because we want to use it later in line 85:
 let resultMyNumber = calculateSum(myNumbers)
 
 // find target for text: 
